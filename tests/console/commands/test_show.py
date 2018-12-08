eb101fb33f3d06bd59db961c67e67e02f9b45414
Sébastien Eustace <sebastien@eustace.io>
2018-12-07 21:00:44 -0500

    Upgrade Cleo to the new improved version

--- tests/console/commands/test_show.py
+++ tests/console/commands/test_show.py
@@ -64,7 +64,7 @@ def test_show_basic_with_installed_packages(app, poetry, installed):
         }
     )
 
-    tester.execute([("command", command.get_name())])
+    tester.execute()
 
     expected = """\
 cachy    0.1.0 Cachy package
@@ -72,7 +72,7 @@ pendulum 2.0.0 Pendulum package
 pytest   3.7.3 Pytest package
 """
 
-    assert tester.get_display(True) == expected
+    assert expected == tester.io.fetch_output()
 
 
 def test_show_basic_with_not_installed_packages_non_decorated(app, poetry, installed):
@@ -119,14 +119,14 @@ def test_show_basic_with_not_installed_packages_non_decorated(app, poetry, insta
         }
     )
 
-    tester.execute([("command", command.get_name())])
+    tester.execute()
 
     expected = """\
 cachy        0.1.0 Cachy package
 pendulum (!) 2.0.0 Pendulum package
 """
 
-    assert tester.get_display(True) == expected
+    assert expected == tester.io.fetch_output()
 
 
 def test_show_basic_with_not_installed_packages_decorated(app, poetry, installed):
@@ -173,14 +173,14 @@ def test_show_basic_with_not_installed_packages_decorated(app, poetry, installed
         }
     )
 
-    tester.execute([("command", command.get_name())], {"decorated": True})
+    tester.execute(decorated=True)
 
     expected = """\
 \033[32mcachy   \033[0m \033[36m0.1.0\033[0m Cachy package
 \033[31mpendulum\033[0m \033[36m2.0.0\033[0m Pendulum package
 """
 
-    assert tester.get_display(True) == expected
+    assert expected == tester.io.fetch_output()
 
 
 def test_show_latest_non_decorated(app, poetry, installed, repo):
@@ -238,14 +238,14 @@ def test_show_latest_non_decorated(app, poetry, installed, repo):
         }
     )
 
-    tester.execute([("command", command.get_name()), ("--latest", True)])
+    tester.execute("--latest")
 
     expected = """\
 cachy    0.1.0 0.2.0 Cachy package
 pendulum 2.0.0 2.0.1 Pendulum package
 """
 
-    assert tester.get_display(True) == expected
+    assert expected == tester.io.fetch_output()
 
 
 def test_show_latest_decorated(app, poetry, installed, repo):
@@ -303,16 +303,14 @@ def test_show_latest_decorated(app, poetry, installed, repo):
         }
     )
 
-    tester.execute(
-        [("command", command.get_name()), ("--latest", True)], {"decorated": True}
-    )
+    tester.execute("--latest", decorated=True)
 
     expected = """\
 \033[32mcachy   \033[0m \033[36m0.1.0\033[0m \033[33m0.2.0\033[0m Cachy package
 \033[32mpendulum\033[0m \033[36m2.0.0\033[0m \033[31m2.0.1\033[0m Pendulum package
 """
 
-    assert tester.get_display(True) == expected
+    assert expected == tester.io.fetch_output()
 
 
 def test_show_outdated(app, poetry, installed, repo):
@@ -367,13 +365,13 @@ def test_show_outdated(app, poetry, installed, repo):
         }
     )
 
-    tester.execute([("command", command.get_name()), ("--outdated", True)])
+    tester.execute("--outdated")
 
     expected = """\
 cachy    0.1.0 0.2.0 Cachy package
 """
 
-    assert tester.get_display(True) == expected
+    assert expected == tester.io.fetch_output()
 
 
 def test_show_hides_incompatible_package(app, poetry, installed, repo):
@@ -422,13 +420,13 @@ def test_show_hides_incompatible_package(app, poetry, installed, repo):
         }
     )
 
-    tester.execute([("command", command.get_name())])
+    tester.execute()
 
     expected = """\
 pendulum 2.0.0 Pendulum package
 """
 
-    assert tester.get_display(True) == expected
+    assert expected == tester.io.fetch_output()
 
 
 def test_show_all_shows_incompatible_package(app, poetry, installed, repo):
@@ -477,14 +475,14 @@ def test_show_all_shows_incompatible_package(app, poetry, installed, repo):
         }
     )
 
-    tester.execute([("command", command.get_name()), ("--all", True)])
+    tester.execute("--all")
 
     expected = """\
 cachy     0.1.0 Cachy package
 pendulum  2.0.0 Pendulum package
 """
 
-    assert tester.get_display(True) == expected
+    assert expected == tester.io.fetch_output()
 
 
 def test_show_non_dev_with_basic_installed_packages(app, poetry, installed):
@@ -548,11 +546,66 @@ def test_show_non_dev_with_basic_installed_packages(app, poetry, installed):
         }
     )
 
-    tester.execute([("command", command.get_name()), ("--no-dev", True)])
+    tester.execute("--no-dev")
 
     expected = """\
 cachy    0.1.0 Cachy package
 pendulum 2.0.0 Pendulum package
 """
 
-    assert tester.get_display(True) == expected
+    assert expected == tester.io.fetch_output()
+
+
+def test_show_tree(app, poetry, installed):
+    command = app.find("show")
+    tester = CommandTester(command)
+
+    poetry.package.add_dependency("cachy", "^0.2.0")
+
+    cachy2 = get_package("cachy", "0.2.0")
+    cachy2.add_dependency("msgpack-python", ">=0.5 <0.6")
+
+    installed.add_package(cachy2)
+
+    poetry.locker.mock_lock_data(
+        {
+            "package": [
+                {
+                    "name": "cachy",
+                    "version": "0.2.0",
+                    "description": "",
+                    "category": "main",
+                    "optional": False,
+                    "platform": "*",
+                    "python-versions": "*",
+                    "checksum": [],
+                    "dependencies": {"msgpack-python": ">=0.5 <0.6"},
+                },
+                {
+                    "name": "msgpack-python",
+                    "version": "0.5.1",
+                    "description": "",
+                    "category": "main",
+                    "optional": False,
+                    "platform": "*",
+                    "python-versions": "*",
+                    "checksum": [],
+                },
+            ],
+            "metadata": {
+                "python-versions": "*",
+                "platform": "*",
+                "content-hash": "123456789",
+                "hashes": {"cachy": [], "msgpack-python": []},
+            },
+        }
+    )
+
+    tester.execute("--tree")
+
+    expected = """\
+cachy 0.2.0
+`-- msgpack-python >=0.5 <0.6
+"""
+
+    assert expected == tester.io.fetch_output()
